b15eeaa35de46b09cbe07f0ca8aad67bcd8af922
Neville Yee <neville.yee@alumni.york.ac.uk>
2017-05-01 14:28:25 +0100

    Julia v.2
    Major restructure of code --- added calculation of colour gradient

--- julia.f90
+++ julia.f90
@@ -6,11 +6,10 @@ function f(z,c)
   complex(kind=dp) :: f
   complex(kind=dp), external :: cmpsinh, cmplog
 
-  f = z**2 + c
+  f = z**5 + c
 end function f
 
 program julia
-!  use hsv_to_rgb
   implicit none
 
   integer, parameter :: dp = selected_real_kind(15,300)
@@ -23,9 +22,11 @@ program julia
 
   complex(kind=dp) :: z, c
   complex(kind=dp), external :: f
-  integer :: i, j, maxiter, maxrad, currstep, maxstep, in_unit, out_unit, colour_unit, ierr
-  real(kind=dp) :: dx, dy, creal, cimag, hue_offset, re_range, im_range, hue
+  integer :: i, j, in_unit, out_unit, grad_unit, colour_unit, ierr
+  integer :: maxiter, maxrad, currstep, maxstep, max_grad, min_grad
+  real(kind=dp) :: dx, dy, creal, cimag, hue_offset, re_range, im_range
   real(kind=dp), dimension(:,:), allocatable :: hsv
+  integer, dimension(:,:), allocatable :: steps, grad
   real(kind=dp), dimension(3) :: hsv_curr
   real(kind=dp), dimension(3) :: hsv_freq
   integer, dimension(3) :: rgb_curr
@@ -60,7 +61,7 @@ program julia
   write(*, 10) 'Canvas domain: (', ranges(1), ',', ranges(2), ') x (', ranges(3), ',', ranges(4), ')'
   10 format(A,F8.5,A,F8.5,A,F8.5,A,F8.5,A)
   
-  ! write out ppm file
+  ! write ppm file headers
   out_unit = 16
   open(out_unit, file='julia.ppm', status='replace', iostat=ierr)
   if (ierr.ne.0) stop 'Error opening julia.ppm in main'
@@ -69,11 +70,25 @@ program julia
   write(out_unit, '(2(I4.4,1X))') pixel(1), pixel(2)
   write(out_unit, '(I3.3)') maxcol
 
-  ! pre-run to determine max number of steps
+  grad_unit = 17
+  open(grad_unit, file='grad.ppm', status='replace', iostat=ierr)
+  if (ierr.ne.0) stop 'Error opening grad.ppm in main'
+
+  write(grad_unit, '(A2)') 'P3'
+  write(grad_unit, '(2(I4.4,1X))') pixel(1)-1, pixel(2)-1
+  write(grad_unit, '(I3.3)') maxcol
+  
+  ! calculations
+  allocate(steps(pixel(2),pixel(1)), stat=ierr)
+  if (ierr.ne.0) stop 'Error allocating steps in main.'
+
+  allocate(grad(2:pixel(2)-1,2:pixel(1)-1), stat=ierr)
+  if (ierr.ne.0) stop 'Error allocating grad in main.'
+
   c = cmplx(creal, cimag, dp)
   dx = (ranges(2)-ranges(1)) / real(pixel(1),dp)
   dy = (ranges(4)-ranges(3)) / real(pixel(2),dp)
-  
+
   maxstep = 0
   do i = 1, pixel(2)
      do j = 1, pixel(1)
@@ -88,18 +103,28 @@ program julia
            end if
            if (abs(z) .gt. maxrad) exit
         end do
+        steps(i,j) = currstep
         if (currstep.gt.maxstep) maxstep = currstep
      end do
   end do
 
-  ! HSV colouring scheme (+ conversion to RGB)
-  allocate(hsv(maxiter, 3), stat=ierr)
+  do i = 2, pixel(2)-1
+     do j = 2, pixel(1)-1
+        grad(i,j) = steps(i+1,j+1) + steps(i-1,j-1) - steps(i-1,j) - steps(i,j-1)
+     end do
+  end do
+  min_grad = minval(grad)
+  max_grad = maxval(grad)
+  print*, min_grad, max_grad
+        
+  ! HSV colouring scheme for julia (+ conversion to RGB)
+  allocate(hsv(maxstep, 3), stat=ierr)
   if (ierr.ne.0) stop 'Error allocating hsv in main.'
 
-  allocate(rgb(maxiter, 3), stat=ierr)
+  allocate(rgb(maxstep, 3), stat=ierr)
   if (ierr.ne.0) stop 'Error allocating rgb in main.'
 
-  colour_unit = 17
+  colour_unit = 18
   open(colour_unit, file='colour', status='replace', iostat=ierr)
   if (ierr.ne.0) stop 'Error opening colour in main'
 
@@ -111,30 +136,13 @@ program julia
      hsv_curr = hsv(i,:)
      call hsv_to_rgb(hsv_curr, rgb_curr, maxcol)
      rgb(i,:) = rgb_curr(:)
-     write(colour_unit, *) i, hsv_curr(:), rgb_curr(:)
+!     write(colour_unit, *) i, hsv_curr(:), rgb_curr(:)
   end do
 
-  ! real calculations
-  c = cmplx(creal, cimag, dp)
-  dx = (ranges(2)-ranges(1)) / real(pixel(1),dp)
-  dy = (ranges(4)-ranges(3)) / real(pixel(2),dp)
-  
+  ! output julia
   do i = 1, pixel(2)
      do j = 1, pixel(1)
-        z = cmplx(ranges(1) + real(j*dx, dp), ranges(4) - real(i*dy, dp), dp)
-        currstep = 0
-        do while (currstep .lt. maxiter)
-           z = f(z,c)
-           currstep = currstep + 1
-           if (abs(z) .lt. 1.0e-15_dp) then
-              currstep = maxiter
-              exit
-           end if
-           if (abs(z) .gt. maxrad) exit
-        end do
-        ! hsv_curr = hsv(currstep,:)
-        ! call hsv_to_rgb(hsv_curr, rgb_curr, maxcol)
-        write(out_unit, '(3(i3.3,1x))') rgb(currstep,:)
+        write(out_unit, '(3(i3.3,1x))') rgb(steps(i,j),:)
      end do
   end do
 
@@ -144,12 +152,51 @@ program julia
   deallocate(rgb, stat=ierr)
   if (ierr.ne.0) stop 'Error deallocating rgb in main.'
   
+  ! HSV colouring scheme for grad (+ conversion to RGB)
+  allocate(hsv(min_grad:max_grad, 3), stat=ierr)
+  if (ierr.ne.0) stop 'Error allocating hsv2 in main.'
+
+  allocate(rgb(min_grad:max_grad, 3), stat=ierr)
+  if (ierr.ne.0) stop 'Error allocating rgb2 in main.'
+
+  hue_offset = hue_offset * pi / 180.0_dp
+  do i = min_grad, max_grad
+     hsv(i,1) = mod(2.0_dp * pi * real(i,dp) * hsv_freq(1) / real(max_grad-min_grad,dp) + hue_offset, 2.0_dp * pi)
+     hsv(i,2) = 0.5_dp * (sin(2.0_dp * pi * i * hsv_freq(2) / real(max_grad-min_grad,dp) - hue_offset) + 1)
+     hsv(i,3) = 0.5_dp * (cos(2.0_dp * pi * i * hsv_freq(3) / real(max_grad-min_grad,dp) - hue_offset) + 1)
+     hsv_curr = hsv(i,:)
+     call hsv_to_rgb(hsv_curr, rgb_curr, maxcol)
+     rgb(i,:) = rgb_curr(:)
+  end do
+
+  ! output grad
+  do i = 2, pixel(2)-1
+     do j = 2, pixel(1)-1
+        write(grad_unit, '(3(i3.3,1x))') rgb(grad(i,j),:)
+     end do
+  end do
+
+  deallocate(hsv, stat=ierr)
+  if (ierr.ne.0) stop 'Error deallocating hsv in main.'
+
+  deallocate(rgb, stat=ierr)
+  if (ierr.ne.0) stop 'Error deallocating rgb in main.'
+
+  deallocate(steps, stat=ierr)
+  if (ierr.ne.0) stop 'Error deallocating steps in main.'
+
+  deallocate(grad, stat=ierr)
+  if (ierr.ne.0) stop 'Error deallocating grad in main.'
+ 
   close(in_unit, iostat=ierr)
   if (ierr.ne.0) stop 'Error closing julia.in in main.'
 
   close(out_unit, iostat=ierr)
   if (ierr.ne.0) stop 'Error closing julia.ppm in main.'
 
+  close(grad_unit, iostat=ierr)
+  if (ierr.ne.0) stop 'Error closing grad.ppm in main.'
+  
   close(colour_unit, iostat=ierr)
   if (ierr.ne.0) stop 'Error closing colour in main.'
   
