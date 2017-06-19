52ef2a1f9c6c583b1b116aa86651f53f903bf877
Neville Yee <neville.yee@alumni.york.ac.uk>
2017-06-19 10:59:35 +0100

    Julia v.4.1
    Change of input method from normal input file. Namelist input now supported.

---
+++ complex_stuffs.mod
@@ -0,0 +1,69 @@
+GFORTRAN module version '10' created from julia.f90
+MD5:c18de64341018d838e697de5a32fc880 -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () () ()
+() () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 'cmplog' 'complex_stuffs' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC
+DECL UNKNOWN 0 0 FUNCTION IMPLICIT_PURE) (COMPLEX 8 0 0 0 COMPLEX ()) 3
+0 (4) () 2 () () () 0 0)
+5 'cmppow' 'complex_stuffs' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC
+DECL UNKNOWN 0 0 FUNCTION IMPLICIT_PURE) (COMPLEX 8 0 0 0 COMPLEX ()) 6
+0 (7 8) () 5 () () () 0 0)
+9 'cmpsinh' 'complex_stuffs' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC
+DECL UNKNOWN 0 0 FUNCTION IMPLICIT_PURE) (COMPLEX 8 0 0 0 COMPLEX ()) 10
+0 (11) () 9 () () () 0 0)
+12 'cmpsq' 'complex_stuffs' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC
+DECL UNKNOWN 0 0 FUNCTION) (COMPLEX 8 0 0 0 COMPLEX ()) 13 0 (14) () 12
+() () () 0 0)
+15 'complex_stuffs' 'complex_stuffs' '' 1 ((MODULE UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () ()
+0 () () () 0 0)
+16 'dp' 'complex_stuffs' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '8') () 0 () () () 0 0)
+17 'f_julia' 'complex_stuffs' '' 1 ((PROCEDURE UNKNOWN-INTENT
+MODULE-PROC DECL UNKNOWN 0 0 FUNCTION IMPLICIT_PURE) (COMPLEX 8 0 0 0
+COMPLEX ()) 18 0 (19 20) () 17 () () () 0 0)
+21 'f_newraph' 'complex_stuffs' '' 1 ((PROCEDURE UNKNOWN-INTENT
+MODULE-PROC DECL UNKNOWN 0 0 FUNCTION) (COMPLEX 8 0 0 0 COMPLEX ()) 22 0
+(23 24) () 21 () () () 0 0)
+25 'pi' 'complex_stuffs' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () (CONSTANT (
+REAL 8 0 0 0 REAL ()) 0 '0.3243f6a8885a30@1') () 0 () () () 0 0)
+26 'selected_real_kind' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 FUNCTION) (UNKNOWN 0 0 0 0 UNKNOWN ())
+0 0 () () 26 () () () 0 0)
+4 'z' '' '' 3 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+7 'z' '' '' 6 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+8 'pow' '' '' 6 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+11 'z' '' '' 10 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+14 'z' '' '' 13 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+19 'z' '' '' 18 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+20 'c' '' '' 18 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+23 'z' '' '' 22 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+24 'c' '' '' 22 ((VARIABLE IN UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 DUMMY) (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () () 0 () () () 0 0)
+)
+
+('cmplog' 0 2 'cmppow' 0 5 'cmpsinh' 0 9 'cmpsq' 0 12 'complex_stuffs' 0
+15 'dp' 0 16 'f_julia' 0 17 'f_newraph' 0 21 'pi' 0 25
+'selected_real_kind' 0 26)
--- julia.f90
+++ julia.f90
@@ -5,15 +5,24 @@ module complex_stuffs
   real(kind=dp), parameter :: pi = 3.141592653589793_dp
 
 contains
-  function f(z,c)
+  function f_julia(z,c) !P(z)
     implicit none
 
     complex(kind=dp), intent(in) :: z, c
-    complex(kind=dp) :: f
+    complex(kind=dp) :: f_julia
 
-    f = z**6 + c
-  end function f
+    f_julia = z**2 + c
+  end function f_julia
 
+  function f_newraph(z,c) ! f(z) = z - P(z)/P'(z)
+    implicit none
+
+    complex(kind=dp), intent(in) :: z, c
+    complex(kind=dp) :: f_newraph
+
+    f_newraph = z - f_julia(z,c) / (-2.0_dp*z*sin(z**2))
+  end function f_newraph
+  
   function cmplog(z)
     implicit none
 
@@ -67,47 +76,43 @@ program julia_all
   integer, dimension(2) :: pixel
 
   real(kind=dp), dimension(4) :: ranges ! (xmin, xmax, ymin, ymax)
+  real(kind=dp), dimension(2) :: cval
 
   complex(kind=dp) :: z, c
-  integer :: i, j, in_unit, out_unit, grad_unit, colour_unit, ierr
-  integer :: maxiter, maxrad, currstep, maxstep, max_grad, min_grad
-  real(kind=dp) :: dx, dy, creal, cimag, hue_offset, re_range, im_range
-  integer, dimension(:,:), allocatable :: steps, grad
+  integer :: i, j, in_unit, out_unit, grad_unit, colour_unit, nr_unit, ierr
+  integer :: max_iter, max_rad, currstep, maxstep, nrstep, max_grad, min_grad, max_nr, min_nr
+  real(kind=dp) :: re_ctr, im_ctr, dx, dy, hue_offset, re_range, im_range, nrlimit
+  integer, dimension(:,:), allocatable :: steps, grad, abs_newraph
   real(kind=dp), dimension(3) :: hsv_curr
   real(kind=dp), dimension(3) :: hsv_freq
   integer, dimension(3) :: rgb_curr
   integer, dimension(2) :: grad_curr
-    real(kind=dp), dimension(:,:), allocatable :: hsv
-    integer, dimension(:,:), allocatable :: rgb
+  real(kind=dp), dimension(:,:), allocatable :: hsv
+  integer, dimension(:,:), allocatable :: rgb
 
-  ! read in parameters
+  namelist /inp/ pixel, re_ctr, im_ctr, re_range, max_iter, max_rad, hsv_freq, hue_offset
+  namelist /param/ cval
+
+  ! read in namelist
   in_unit = 15
-  open(in_unit, file='julia.in', status='old', iostat=ierr)
-  if (ierr.ne.0) stop 'Error opening julia.in in main.'
+  open(in_unit, file='nml_julia', status='old', iostat=ierr)
+  if (ierr.ne.0) stop 'Error opening nml_julia in main.'
 
-  read(15, *)
-  read(15, *) pixel(1), pixel(2)
-  read(15, *)
-  read(15, *)
-  read(15, *) ranges(1), ranges(3), re_range
-  read(15, *)
-  read(15, *)
-  read(15, *) maxiter, maxrad
-  read(15, *)
-  read(15, *)
-  read(15, *) hsv_freq(1), hsv_freq(2), hsv_freq(3), hue_offset
-  read(15, *)
-  read(15, *)
-  read(15, *) creal, cimag
-
+  read(15, nml=inp)
+  read(15, nml=param)
+  
   ! determine max values of Re(z) and Im(z)
-  ranges(2) = ranges(1) + 0.5_dp * re_range
-  ranges(1) = ranges(2) - re_range
+  ranges(1) = re_ctr - 0.5_dp * re_range
+  ranges(2) = re_ctr + 0.5_dp * re_range
   im_range = re_range * real(pixel(2),dp) / real(pixel(1),dp)
-  ranges(4) = ranges(3) + 0.5_dp * im_range
-  ranges(3) = ranges(4) - im_range
+  ranges(3) = im_ctr - 0.5_dp * im_range
+  ranges(4) = im_ctr + 0.5_dp * im_range
   write(*, 10) 'Canvas domain: (', ranges(1), ',', ranges(2), ') x (', ranges(3), ',', ranges(4), ')'
-  write(*, 11) 'C = ', creal, ' +', cimag, 'i'
+  if (cval(2).lt.0) then
+     write(*, 11) 'C = ', cval(1), ' -', abs(cval(2)), 'i'
+  else
+     write(*, 11) 'C = ', cval(1), ' +', cval(2), 'i'
+  end if
 10 format(A,F8.5,A,F8.5,A,F8.5,A,F8.5,A)
 11 format(A,F8.5,A,F8.5,A)
 
@@ -128,6 +133,18 @@ program julia_all
   write(grad_unit, '(2(I4.4,1X))') pixel(1)-2, pixel(2)-2
   write(grad_unit, '(I3.3)') maxcol
 
+  nr_unit = 18
+  open(nr_unit, file='newton.ppm', status='replace', iostat=ierr)
+  if (ierr.ne.0) stop 'Error opening newton.ppm in main'
+
+  write(nr_unit, '(A2)') 'P3'
+  write(nr_unit, '(2(I4.4,1X))') pixel(1), pixel(2)
+  write(nr_unit, '(I3.3)') maxcol
+
+  colour_unit = 19
+  open(colour_unit, file='colour', status='replace', iostat=ierr)
+  if (ierr.ne.0) stop 'Error opening colour in main'
+
   ! calculations
   allocate(steps(pixel(2),pixel(1)), stat=ierr)
   if (ierr.ne.0) stop 'Error allocating steps in main.'
@@ -135,26 +152,29 @@ program julia_all
   allocate(grad(2:pixel(2)-1,2:pixel(1)-1), stat=ierr)
   if (ierr.ne.0) stop 'Error allocating grad in main.'
 
-  c = cmplx(creal, cimag, dp)
+  allocate(abs_newraph(pixel(2),pixel(1)), stat=ierr)
+  if (ierr.ne.0) stop 'Error allocating abs_newraph in main.'
+
+  c = cmplx(cval(1), cval(2), dp)
   dx = (ranges(2)-ranges(1)) / real(pixel(1),dp)
   dy = (ranges(4)-ranges(3)) / real(pixel(2),dp)
 
   !$OMP PARALLEL DO &
   !$OMP DEFAULT(NONE) &
   !$OMP PRIVATE(i,j,z,currstep) &
-  !$OMP SHARED(c,pixel,ranges,dx,dy,maxiter,maxrad,steps)
+  !$OMP SHARED(c,pixel,ranges,dx,dy,max_iter,max_rad,steps)
    do i = 1, pixel(2)
      do j = 1, pixel(1)
         z = cmplx(ranges(1) + real(j*dx, dp), ranges(4) - real(i*dy, dp), dp)
         currstep = 0
-        do while (currstep .lt. maxiter)
-           z = f(z,c)
+        do while (currstep .lt. max_iter)
+           z = f_julia(z,c)
            currstep = currstep + 1
            if (abs(z) .lt. 1.0e-15_dp) then
-              currstep = maxiter
+              currstep = max_iter
               exit
            end if
-           if (abs(z) .gt. maxrad) exit
+           if (abs(z) .gt. max_rad) exit
         end do
         steps(i,j) = currstep
      end do
@@ -171,14 +191,39 @@ program julia_all
   min_grad = minval(grad)
   max_grad = maxval(grad)
 
+  ! !$OMP PARALLEL DO &
+  ! !$OMP DEFAULT(NONE) &
+  ! !$OMP PRIVATE(i,j,z,currstep) &
+  ! !$OMP SHARED(c,pixel,ranges,dx,dy,nrstep,abs_newraph,nrlimit)
+  ! do i = 1, pixel(2)
+  !    do j = 1, pixel(1)
+  !       z = cmplx(ranges(1) + real(j*dx, dp), ranges(4) - real(i*dy, dp), dp)
+  !       do currstep = 1, nrstep
+  !          z = f_newraph(z,c)
+  !          if (abs(aimag(z)).gt.1.0e10_dp) then
+  !             z = cmplx(real(nrlimit,dp),0.0_dp,dp)
+  !             exit
+  !          end if
+  !       end do
+  !       abs_newraph(i,j) = nint(abs(z))
+  !    end do
+  ! end do
+  ! !$OMP END PARALLEL DO
+  ! min_nr = minval(abs_newraph)
+  ! max_nr = maxval(abs_newraph)
+
   call julia_out()
   call equipot_out()
+  ! call nr_out()
 
   deallocate(steps, stat=ierr)
   if (ierr.ne.0) stop 'Error deallocating steps in main.'
 
   deallocate(grad, stat=ierr)
   if (ierr.ne.0) stop 'Error deallocating grad in main.'
+
+  deallocate(abs_newraph, stat=ierr)
+  if (ierr.ne.0) stop 'Error deallocating abs_newraph in main.'
 
   close(in_unit, iostat=ierr)
   if (ierr.ne.0) stop 'Error closing julia.in in main.'
@@ -200,10 +245,6 @@ contains
 
     allocate(rgb(maxstep, 3), stat=ierr)
     if (ierr.ne.0) stop 'Error allocating rgb in main.'
-
-    colour_unit = 18
-    open(colour_unit, file='colour', status='replace', iostat=ierr)
-    if (ierr.ne.0) stop 'Error opening colour in main'
 
     hue_offset = hue_offset * pi / 180.0_dp
     do i = 1, maxstep
@@ -263,6 +304,42 @@ contains
     if (ierr.ne.0) stop 'Error deallocating rgb in main.'
   end subroutine equipot_out
 
+  subroutine nr_out()
+    ! HSV colouring scheme for Newton-Raphson (+ conversion to RGB)
+    allocate(hsv(min_grad:max_grad, 3), stat=ierr)
+    if (ierr.ne.0) stop 'Error allocating hsv2 in main.'
+
+    allocate(rgb(min_grad:max_grad, 3), stat=ierr)
+    if (ierr.ne.0) stop 'Error allocating rgb2 in main.'
+
+    print*, min_nr, max_nr
+    do i = min_nr, max_nr
+       hsv(i,1) = mod(2.0_dp * pi * real(i-min_nr,dp) * hsv_freq(1) / real(max_nr-min_nr,dp) &
+            & , 2.0_dp * pi)
+       hsv(i,2) = 0.5_dp * (sin(2.0_dp * pi * real(i-min_nr,dp) * hsv_freq(2) / real(max_nr-min_nr,dp) &
+            & ) + 1)
+       hsv(i,3) = 0.5_dp * (cos(2.0_dp * pi * real(i-min_nr,dp) * hsv_freq(3) / real(max_nr-min_nr,dp) &
+            & ) + 1)
+       hsv_curr = hsv(i,:)
+       call hsv_to_rgb(hsv_curr, rgb_curr, maxcol)
+       rgb(i,:) = rgb_curr(:)
+       write(colour_unit,*) i, rgb_curr(:)
+    end do
+
+    ! output Newton-Raphson
+    do i = 1, pixel(2)
+       do j = 1, pixel(1)
+          write(nr_unit, '(3(i3.3,1x))') rgb(abs_newraph(i,j),:)
+       end do
+    end do
+
+    deallocate(hsv, stat=ierr)
+    if (ierr.ne.0) stop 'Error deallocating hsv in main.'
+
+    deallocate(rgb, stat=ierr)
+    if (ierr.ne.0) stop 'Error deallocating rgb in main.'
+  end subroutine nr_out
+  
 end program julia_all
 
 subroutine hsv_to_rgb (hsv_array, rgb_array, maxcol)
